e4fe717b9119d24af1969fa87f4644d371257a37
Heidarian <Alexander.Heidarian@parks.ca.gov>
2019-06-13 16:46:31 -0700

    Fixed flickering effect from first screen to second; set up transition of questions form

--- animations.js
+++ animations.js
@@ -1,28 +1,32 @@
 //JS code for managing user interactions for prototype
-var loginBtn = document.querySelector(".login-btn");
+var reportBtn = document.querySelector(".report-btn");
 var header = document.querySelector(".header");
-var loginForm = document.querySelector(".loginForm");
+var question = document.querySelector(".container-questions");
 
 //content for the login screen
-var loginScreen = [header, loginForm];
+var welcomeScreen = [header];
 
 //Stuff to do when user clicks the login button
-loginBtn.addEventListener('click', function() {
+reportBtn.addEventListener('click', function() {
   (function() {
     //apply the screenTransition keyframe animation to each element in the login screen
-    loginScreen.forEach(function(el) {
+    welcomeScreen.forEach(function(el) {
       el.style.animation = "screenTransition 1.0s forwards";
     });
     //overwrite the animation of the login button
-    loginBtn.style.animationName = "none";
-    loginBtn.style.animation = "screenTransition 1.0s forwards";
+    reportBtn.style.animationName = "none";
+    reportBtn.style.animation = "screenTransition 1.0s forwards";
+
   })();
 
   //When user clicks the login button, scale the overlay to make room for the options menu
   //Also stop the wave animation
   setTimeout(function() {
-    var overlay = document.querySelector(".container");
+    var overlay = document.querySelector(".container-welcome");
+    var bg = document.querySelector(".mainframe");
     overlay.style.animation = "moveWater 0.5s forwards";
+    bg.style.animation = "changeBG 0.5s forwards";
+    question.style.animation = "questionsTransition 0.5s 0.5s forwards";
   }, 200);
 });
 
